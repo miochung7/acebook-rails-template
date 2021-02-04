2b297a12fe5cd0fb4bfcc5d0d72e93b440c11a27
rayhan <rayhanm@hotmail.co.uk>
2021-02-04 17:10:54 +0000

    models for friendship created

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,28 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :posts, dependent: :destroy
+
+  has_many :friend_requests_as_requestor,
+    foreign_key: :requestor_id,
+    class_name: :FriendRequest
+
+  has_many :friend_requests_as_receiver,
+    foreign_key: :receiver_id,
+    class_name: :FriendRequest
+
+  has_many :friendships_as_friend_a
+    foreign_key: :friend_a_id,
+    class_name: :Friendship
+
+  has_many :friendships_as_friend_b
+    foreign_key: :friend_b_id,
+    class_name: :Friendship
+
+  has_many :friend_as, through: :friendships_as_friend_b
+  has_many :friend_bs, through: :friendships_as_friend_a
+
+  def friendships
+    self.friendships_as_friend_a + self.friendships_as_friend_b
+  end
+
 end
